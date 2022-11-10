f929a74539741425d6d33231630c69e7adabf682
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-10 15:11:49 +0500

    added some styles

--- src/Question.js
+++ src/Question.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
   return (
-    <article>
-      <header></header>
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn">btn</button>
+      </header>
     </article>
   );
 };
